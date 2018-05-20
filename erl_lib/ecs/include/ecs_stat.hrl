0175442aa609c36996704acc699ce6efc09100ec
Marshall Clyburn <mardev2013@gmail.com>
2018-05-19 21:11:32 -0400

    Add default values to stat record

--- erl_lib/ecs/include/ecs_stat.hrl
+++ erl_lib/ecs/include/ecs_stat.hrl
@@ -1,9 +1,9 @@
--record(stat, {name :: nonempty_string(),
-               value :: number(),
-               tags :: [nonempty_string()],
-               time :: integer(),
-               origin :: atom(),
-               options :: #{atom() => term()}}).
+-record(stat, {name = "" :: nonempty_string(),
+               value = 0 :: number(),
+               tags = [] :: [nonempty_string()],
+               time = 0 :: integer(),
+               origin = unknown :: atom(),
+               options = #{} :: #{atom() => term()}}).
 
 -type stat() :: #stat{}.
 -type stat_option() :: {continuous, boolean()}
